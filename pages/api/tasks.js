f9c49e887b5cf8bc4a97d0932becc4a69c336928
Mathias Angerer <>
2022-03-29 10:37:13 +0200

    Update api

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -1,3 +1,4 @@
+import { request } from 'http';
 import { NextApiRequest, NextApiResponse } from 'next';
 import {
   getTasks,
@@ -6,6 +7,29 @@ import {
   deleteTaskById,
 } from '../../util/database';
 
-const baseUrl = 'http://localhost:4000';
-const response = await fetch(`${baseUrl}/tasks`);
-const allTasks = await response.json();
+export default function handler(req, res) {
+  //all tasks
+  if (request.method === 'GET') {
+    const tasks = getTasks();
+    res.status(200).json(tasks);
+    //new task
+  } else if (request.method === 'POST') {
+    let taskFromRequest = request.body;
+    const newTask = await creatTask(
+      (taskFromRequest = request.name),
+      (taskFromRequest = request.points),
+    );
+    res.status(200).json(newTask);
+    //update task
+  } else if (request.method === 'UPDATE') {
+    const tasks = updateTaskById();
+    res.status(200).json(tasks);
+  }
+  //delete task
+  if (request.method === 'DELETE');
+  {
+    const deletedTask = await deleteTaskById(taskId);
+    res.status(200).json(deletedTask);
+  }
+  res.status(405).json({ error: 'Method not Allowed' });
+}
